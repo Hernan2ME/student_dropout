f6db155095b3848edeaee2abae0825ba462f8198
Hernán Manrique <hernanmanuelespindola@gmail.com>
2025-11-10 08:27:19 -0500

    extender los atributos de desercion

--- analysis.py
+++ analysis.py
@@ -1,5 +1,15 @@
+import pandas as pd
+
 def dropout_rate(df):
   total = len(df)
   dropped = len(df[df["status"] == "Dropped"])
   rate = dropped / total * 100
+
+
   return round(rate, 2)
+
+def dropout_cause(df):
+    causa = df["cause"].value_counts().idxmax()
+
+    return causa
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from data_loader import load_data
-from analysis import dropout_rate
+from analysis import dropout_rate, dropout_cause
 
 if __name__ == "__main__":
   data = load_data()
   rate = dropout_rate(data)
+  causes = dropout_cause(data)
   print(f"Dropout rate: {rate}%")
+  print(f"Dropout causes: {causes}")
